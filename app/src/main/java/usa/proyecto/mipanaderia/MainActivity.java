5334f6d08a01e6382e1cc1d33f0c5440c4b7080a
Edwin Eslover Romero Fonseca <edwin.romero.mt@correo.usa.edu.co>
2021-11-29 22:06:25 -0500

    primer commit

---
+++ app/src/main/java/usa/proyecto/mipanaderia/MainActivity.java
@@ -0,0 +1,49 @@
+package usa.proyecto.mipanaderia;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menuopciones, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.productosId){
+            Toast.makeText(getApplicationContext(), "Productos", Toast.LENGTH_LONG).show();
+            Intent pantallaProductos = new Intent(this, Productos.class);
+            startActivity(pantallaProductos);
+        }
+        if(id == R.id.serviciosId){
+            Toast.makeText(getApplicationContext(), "Servicios", Toast.LENGTH_LONG).show();
+            Intent pantallaServicios = new Intent(this, Servicios.class);
+            startActivity(pantallaServicios);
+        }
+        if(id == R.id.sucursalesId){
+            Toast.makeText(getApplicationContext(), "Sucursales", Toast.LENGTH_LONG).show();
+            Intent pantallaSucursales = new Intent(this, Sucursales.class);
+            startActivity(pantallaSucursales);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
