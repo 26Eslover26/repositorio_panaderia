d4b6474eed5df1756f195eec23ed30300d4c59f1
Edwin Eslover Romero Fonseca <edwin.romero.mt@correo.usa.edu.co>
2021-12-03 20:24:04 -0500

    se le incluyó una descripción a los productos

--- app/src/main/java/usa/proyecto/mipanaderia/Productos.java
+++ app/src/main/java/usa/proyecto/mipanaderia/Productos.java
@@ -9,15 +9,32 @@ import android.widget.ImageView;
 
 public class Productos extends AppCompatActivity {
 
+    Drawable drawable, drawable1, drawable2, drawable3;
+    ImageView pan1, pan2, pan3, pan4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_productos);
 
         Resources res = getResources();
-        Drawable drawable = res.getDrawable(R.drawable.pan10, getTheme());
-
-        ImageView pan1 = findViewById(R.id.pan1);
+        drawable = res.getDrawable(R.drawable.pan10, getTheme());
+        pan1 = findViewById(R.id.pan1);
         pan1.setImageDrawable(drawable);
+
+        Resources res1 = getResources();
+        drawable1 = res1.getDrawable(R.drawable.pan20, getTheme());
+        pan2 = findViewById(R.id.pan2);
+        pan2.setImageDrawable(drawable1);
+
+        Resources res2 = getResources();
+        drawable2 = res2.getDrawable(R.drawable.pan30, getTheme());
+        pan3 = findViewById(R.id.pan3);
+        pan3.setImageDrawable(drawable2);
+
+        Resources res3 = getResources();
+        drawable3 = res3.getDrawable(R.drawable.pan40, getTheme());
+        pan4 = findViewById(R.id.pan4);
+        pan4.setImageDrawable(drawable3);
     }
 }
